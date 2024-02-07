be78fc6eec0f2499a5ea85d60f879a6fea081772
Rachael Phillips <rachael.aphillips@outlook.com>
2024-02-07 11:42:25 -0500

    saving the dates start file

--- Ch4 - Dates and Times/dates_start.py
+++ Ch4 - Dates and Times/dates_start.py
@@ -3,25 +3,32 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
-
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 def main():
     ## DATE OBJECTS
     # TODO: Get today's date from the simple today() method from the date class
+    # today = date.today()
+    # print("Today's date is", today)
 
-
-    # TODO: print out the date's individual components
-
+    # # TODO: print out the date's individual components
+    # print("Date components", today.day, today.month, today.year)
     
-    # TODO: retrieve today's weekday (0=Monday, 6=Sunday)
-
+    # # TODO: retrieve today's weekday (0=Monday, 6=Sunday)
+    # print("Today's weekday number is", today.weekday())
+    # days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+    # print("Which is a ", days[today.weekday()])
     
     ## DATETIME OBJECTS
     # TODO: Get today's date from the datetime class
-
+    today = datetime.now()
+    print("The current date and time is ", today)
     
     # TODO: Get the current time
-
+    t = datetime.time(datetime.now())
+    print("The current time is", t)
  
 
   
